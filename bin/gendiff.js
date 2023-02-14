12620d8ca58025fa453b0fe5e0bb928dc3a952f5
Ilialuck <ilialuck@yandex.ru>
2023-02-14 18:46:48 +0300

    add arguments and option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,5 +5,8 @@ import { program } from 'commander'
 program
     .version('0.1.0')
     .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format <type>', 'output format')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
 
 program.parse();
