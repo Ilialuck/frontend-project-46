ab26aca84525d81af84ba304e798e716f24dbf77
Ilialuck <ilialuck@yandex.ru>
2023-08-24 21:01:54 +0300

    step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
+
 
 const program = new Command();
 
@@ -9,5 +11,8 @@ program
     .version('1.0.0')
     .option('-f, --format <type>', 'output format')
     .arguments('<filepath1> <filepath2>')
+    .action((filepath1, filepath2) => {
+        console.log(genDiff(filepath1, filepath2));
+      })
 
-program.parse();
+    .parse();
