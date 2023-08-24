5ee9851c886092a0081ab4e98d86761bfff09a3e
Ilialuck <ilialuck@yandex.ru>
2023-08-24 22:01:33 +0300

    add lint

--- src/index.js
+++ src/index.js
@@ -3,35 +3,35 @@ import { readFileSync } from 'fs';
 import path from 'path';
 
 const getFileData = (filename) => {
-    const filePath = path.resolve(process.cwd(), filename);
-    const data = readFileSync(filePath, 'utf-8');
-    return data;
+  const filePath = path.resolve(process.cwd(), filename);
+  const data = readFileSync(filePath, 'utf-8');
+  return data;
 };
 
 const genDiff = (filepath1, filepath2) => {
-    const data1 = getFileData(filepath1)
-    const data2 = getFileData(filepath2)
-  
-    const obj1 = JSON.parse(data1);
-    const obj2 = JSON.parse(data2);
-  
-    const keys = _.union(Object.keys(obj1), Object.keys(obj2));
-    const sortedKeys = _.sortBy(keys);
-    const differences = sortedKeys.map((key) => {
-      if (!_.has(obj1, key)) {
-        return `  + ${key}: ${obj2[key]}`;
-      }
-      if (!_.has(obj2, key)) {
-        return `  - ${key}: ${obj1[key]}`;
-      }
-      if (obj1[key] === obj2[key]) {
-        return `    ${key}: ${obj1[key]}`;
-      }
-      return `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`;
-    });
-  
-    const result = `{\n${differences.join('\n')}\n}`;
-    return result;
-  };
+  const data1 = getFileData(filepath1);
+  const data2 = getFileData(filepath2);
+
+  const obj1 = JSON.parse(data1);
+  const obj2 = JSON.parse(data2);
+
+  const keys = _.union(Object.keys(obj1), Object.keys(obj2));
+  const sortedKeys = _.sortBy(keys);
+  const differences = sortedKeys.map((key) => {
+    if (!_.has(obj1, key)) {
+      return `  + ${key}: ${obj2[key]}`;
+    }
+    if (!_.has(obj2, key)) {
+      return `  - ${key}: ${obj1[key]}`;
+    }
+    if (obj1[key] === obj2[key]) {
+      return `    ${key}: ${obj1[key]}`;
+    }
+    return `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`;
+  });
+
+  const result = `{\n${differences.join('\n')}\n}`;
+  return result;
+};
 
 export default genDiff;
