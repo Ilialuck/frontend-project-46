1021141862af9f0e274b63f937ebec94e0f4e87c
Ilialuck <ilialuck@yandex.ru>
2023-09-12 11:42:35 +0300

    first refactoring -6

--- src/buildTree.js
+++ src/buildTree.js
@@ -4,7 +4,7 @@ const buildDifferencesTree = (data1, data2) => {
   const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
 
   const diff = keys.map((key) => {
-    if (_.isObject(data1[key]) && _.isObject(data2[key])) {
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
       return { key, children: buildDifferencesTree(data1[key], data2[key]), type: 'nested' };
     }
     if (!Object.hasOwn(data1, key)) {
@@ -13,7 +13,7 @@ const buildDifferencesTree = (data1, data2) => {
     if (!Object.hasOwn(data2, key)) {
       return { key, value1: data1[key], type: 'deleted' };
     }
-    if (data1[key] !== data2[key]) {
+    if (!_.isEqual(data1[key], data2[key])) {
       return {
         key, value1: data1[key], value2: data2[key], type: 'changed',
       };
